053a52c53a792915560aeec3ba1aaf75e5cb3140
glauberguedes <glauber-94@hotmail.com>
2021-07-26 18:21:41 -0300

    base do front-end

---
+++ desafio-05/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
